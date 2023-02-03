941a9a14072f9a3d310488d7be595ff3517b94b9
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2023-02-03 16:37:52 +0200

    fixes

--- _friends/schema.js
+++ _friends/schema.js
@@ -1,38 +1,30 @@
-const { string } = require('joi');
 const mongoose = require('mongoose');
 
 const friendSchema = new mongoose.Schema({
-  _id: Number,
-  logo: {
-    type: Buffer,
-    default: ""
-  },
+  logo: String,
   name: String,
   friendUrl: String,
   workingHours: [
     {
       day: {
         type: String,
-        enum: ['MN', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
+        enum: ['MN', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
       },
       from: {
         type: String,
-         default: "-"
+        default: '-',
       },
       to: {
         type: String,
-        default: "-"
-      }
-    }
+        default: '-',
+      },
+    },
   ],
   adress: String,
   adressUrl: String,
   email: String,
   phone: String,
-  
-  
 });
-
 
 module.exports = {
   friendSchema,
--- _news/controllers.js
+++ _news/controllers.js
@@ -1,88 +1,85 @@
-const mongoose = require("mongoose");
-const { getNewID } = require("../Helpers/newID.js");
+const mongoose = require('mongoose');
+// const { getNewID } = require('../Helpers/newID.js');
 
-
-const { newSchema } = require("./schema.js");
-const News = mongoose.model("News", newSchema);
+const { newSchema } = require('./schema.js');
+const News = mongoose.model('News', newSchema);
 
 async function getNews(req, res) {
-
-  await News.find()
-    .exec((err, news) => {
-      if (err) {
-        res.status(500).json({ "message": err });
-        return;
-      }
-      res.json({
-        message: 'Success',
-        data: news
-      })
-    })  
-}
-
-async function addNews(req, res) {
-  const _id = await getNewID(News);
-  const news = req.body;
-  const newNews = new News({ _id, ...news});
-  
-  newNews.save(async (err, news) => {    
+  await News.find().exec((err, news) => {
     if (err) {
-      res.status(500).json({ message: "Error occurred", "err": err });
+      res.status(500).json({ message: err });
       return;
     }
     res.json({
-      message: "News added",
-      data: news,         
-    });                 
+      message: 'Success',
+      data: news,
+    });
+  });
+}
+
+async function addNews(req, res) {
+  // const _id = await getNewID(News);
+  const news = req.body;
+  const newNews = new News({ ...news });
+
+  newNews.save(async (err, news) => {
+    if (err) {
+      res.status(500).json({ message: 'Error occurred', err: err });
+      return;
+    }
+    res.json({
+      message: 'News added',
+      data: news,
+    });
   });
 }
 
 async function removeNews(req, res) {
   const _id = req.params.id;
-  await News.findOneAndDelete({_id})
-    .exec((err, pet) => {
-        if (err) {
-          res.status(500).json({ message: "Error occured", err: err  });
-          return;
-        }
-        res.json({
-          message: "Delete success",
-          data: pet
-        })      
-      })
-
+  await News.findOneAndDelete({ _id }).exec((err, pet) => {
+    if (err) {
+      res.status(500).json({ message: 'Error occured', err: err });
+      return;
+    }
+    res.json({
+      message: 'Delete success',
+      data: pet,
+    });
+  });
 }
 
 async function updateNews(req, res) {
   const _id = req.params.id;
   const prop = req.body;
-  await News.findOneAndUpdate({_id}, { $set: { ...prop } }, {returnDocument: 'after'})
-    .exec((err, news) => {
-        if (err) {
-        res.status(500).json({ message: "Error occured", err: err });
-        return;
-      }
-       res.json({
-        message: "Update success",
-        data: news
-      })      
-    })
+  await News.findOneAndUpdate(
+    { _id },
+    { $set: { ...prop } },
+    { returnDocument: 'after' }
+  ).exec((err, news) => {
+    if (err) {
+      res.status(500).json({ message: 'Error occured', err: err });
+      return;
+    }
+    res.json({
+      message: 'Update success',
+      data: news,
+    });
+  });
 }
 
 async function searchNews(req, res) {
-    
   const docs = await News.aggregate([
-  {
-    '$search': {
-      'index': 'default',
-      'text': {
-        'query': req.params.str,
-        'path': ["text", "title"]
-      }
-    }
-  }
-]);
-  res.json({ message: "Search result", data: docs });
+    {
+      $search: {
+        index: 'default',
+        text: {
+          query: req.params.str,
+          path: ['text', 'title'],
+        },
+      },
+    },
+  ]);
+  res.json({ message: 'Search result', data: docs });
 }
 
 module.exports = {
